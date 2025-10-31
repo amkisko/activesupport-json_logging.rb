16a8f5a1edf1aa2a87964178927b823a4f004253
Andrei Makarov <andrei@kiskolabs.com>
2025-10-31 20:37:36 +0200

    Add development dependencies for testing and coverage reporting
    - Include `rspec_junit_formatter` and `simplecov-cobertura` in the gemspec for enhanced test reporting.
    - Update `Gemfile.lock` to reflect the new dependencies.
    - Introduce a new GitHub Actions workflow for trunk checks.
    - Enhance the CI workflow to include coverage reporting steps and utilize the new formatter.
    - Configure RSpec to use the JUnit formatter for CI environments.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,15 @@ SimpleCov.start do
   add_filter { |source_file| source_file.lines.count < 5 }
 end
 
+require "simplecov-cobertura"
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
 require "rspec"
 require "json"
 require_relative "../lib/json_logging"
 
+Dir[File.expand_path("support/**/*.rb", __dir__)].each { |f| require_relative f }
+
 RSpec.configure do |config|
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+
 end
